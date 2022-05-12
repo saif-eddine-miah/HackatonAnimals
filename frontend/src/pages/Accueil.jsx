992fcfea47dfbf525448972bff332bebf870c802
francois_pobelle <francoispobelle@hotmail.fr>
2022-05-12 16:22:30 +0200

    pages accueil

--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import LogoTinder from "../assets/logosite.svg";
+import "./accueil.css";
 
 function Accueil() {
-  return <div>Accueil</div>;
+  return (
+    <div className="logo-inscription">
+      <img className="logo-site" src={LogoTinder} alt="logo tinder animal" />
+      <button className="inscription" type="button">
+        Inscription
+      </button>
+    </div>
+  );
 }
 
 export default Accueil;
