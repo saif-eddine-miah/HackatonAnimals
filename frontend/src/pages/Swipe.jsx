a28acf23104d89978acc08a7738bd35de71598bf
Antoine Dabonneville <antoine.dabonneville@pm.me>
2022-05-12 23:03:10 +0200

    add call DB

--- frontend/src/pages/Swipe.jsx
+++ frontend/src/pages/Swipe.jsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import ProfileCard from "../components/ProfileCard";
 
 function Swipe() {
+  const [animals, setAnimals] = useState([]);
+  useEffect(() => {
+    const url = "http://localhost:5000/animals";
+    axios.get(url).then((res) => setAnimals(res.data));
+  }, []);
+
+  const animal = animals[0];
   return (
     <div>
-      <ProfileCard />;
+      <ProfileCard name={animal.animalName} />;
     </div>
   );
 }
