d7d6579e0f97d764bc7bdbc4c0f9d0ddead960ef
francois_pobelle <francoispobelle@hotmail.fr>
2022-05-12 15:11:06 +0200

    create routes and pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
-import Home from "./pages/Home";
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Accueil from "./pages/Accueil";
+import Match from "./pages/Match";
+import Tchat from "./pages/Tchat";
 import "./App.css";
+import Swipe from "./pages/Swipe";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Accueil />} />
+          <Route path="/users" element={<Swipe />} />
+          <Route path="/users" element={<Match />} />
+          <Route path="/users" element={<Tchat />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
